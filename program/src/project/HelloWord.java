153298635abb990f71f3ea874245635a81488aa0
GOKUL <GOKUL@DESKTOP-BHQF87V>
2024-10-24 11:42:55 +0530

    commited

---
+++ program/src/project/HelloWord.java
@@ -0,0 +1,19 @@
+package project;
+
+public class HelloWord
+{
+	public void print()
+	{
+		System.out.println("hiii");
+	}
+
+	public static void main(String[] args)
+	{
+		HelloWord hw=new HelloWord();
+		hw.print();
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ program/src/project/InstanceExample.java
@@ -0,0 +1,23 @@
+package project;
+
+public class InstanceExample 
+{
+	public void print(int m)
+	{
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	
+	}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		InstanceExample ie=new InstanceExample();
+		ie.print(10);
+		
+
+	}
+
+}
